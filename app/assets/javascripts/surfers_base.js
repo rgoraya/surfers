ec6ff3e87f14135a4e9ff0f485509226949a0a5f
Raminder Goraya <raminder@Raminders-MacBook-Pro.local>
2015-08-12 01:09:57 -0700

    adding jquery mobile support

--- app/assets/javascripts/surfers_base.js
+++ app/assets/javascripts/surfers_base.js
@@ -28,8 +28,12 @@ $(document).ready(function() {
     });
 
     $(".carousel")
-        .on("swiperight", function() { $(this).carousel('prev'); })
-        .on("swipeleft", function() { $(this).carousel('next'); });
+        .on("swiperight", function() {
+            $(this).carousel('prev');
+        })
+        .on("swipeleft", function() {
+            $(this).carousel('next');
+        });
 
     if (typeof picasa_album_id !== 'undefined' && typeof picasa_user_id !== 'undefined') {
         if (typeof picasa_content_template !== 'undefined') {
