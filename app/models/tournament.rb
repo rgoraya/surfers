40c8687155ca8c3ba4b1a392177f4e2d4dce951e
Raminder Goraya <raminder@Raminders-MacBook-Pro.local>
2017-06-19 15:29:36 -0700

    sorting registrations for tournament by date and adding date string

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,7 +1,7 @@
 class Tournament < ActiveRecord::Base
   attr_accessible :slug, :start_time, :end_time, :name, :location, :description, :registration_deadline
 
-  has_many :tournament_registrations
+  has_many :tournament_registrations, :order => "created_at DESC"
   has_many :teams, :through => :tournament_registrations, :readonly => false
 
   has_many :tournament_divisions
